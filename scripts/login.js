0a56368168a201c704cdef00d5852ce8640a3acd
Sein <seinprince2@gmail.com>
2023-10-24 18:35:40 +0200

    made logins and register send to ParseSdk

--- scripts/login.js
+++ scripts/login.js
@@ -1,35 +1,92 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
-const loginBtn = document.getElementById("login-btn");
+document.addEventListener("DOMContentLoaded", function () {
 
-// Define the predefined username and password
-const predefinedUsername = "seinprince2@gmail.com";
-const predefinedPassword = "14781";
+  const sign_in_btn = document.querySelector("#sign-in-btn");
+  const sign_up_btn = document.querySelector("#sign-up-btn");
+  const container = document.querySelector(".container");
 
-// Function to handle login
-function handleLogin() {
-  const usernameInput = document.getElementById("username").value;
-  const passwordInput = document.getElementById("password").value;
 
-  // Check if the entered details match the predefined values
-  if (usernameInput === predefinedUsername && passwordInput === predefinedPassword) {
-    // Login successful, you can redirect the user to another page or show a success message
-    window.location.href = "dashboard.html";
-    alert("Login successful!");
-  } else {
-    // Login failed, display an error message
-    alert("Invalid username or password. Please try again.");
+  Parse.initialize("Cs3njaNlyqestAvd0G4GhMAbPFf0JYEGiX8wzDIz", "ZuuVvlVviBeHf4fnX0Dgz3LX5Oh0NmFIxSVOZki3");
+  Parse.serverURL = "https://parseapi.back4app.com/";
+  // Your existing code to handle the signup/sign-in form toggle
+
+  const loginBtn = document.getElementById("login-btn");
+  const signupBtn = document.getElementById("signup-btn");
+
+  const loginUsernameInput = document.getElementById("login-username");
+  const loginPasswordInput = document.getElementById("login-password");
+
+  const signupUsernameInput = document.getElementById("signup-username");
+  const signupEmailInput = document.getElementById("signup-email");
+  const signupPasswordInput = document.getElementById("signup-password");
+
+  function handleLogin(event) {
+    event.preventDefault();
+  
+    const username = loginUsernameInput.value;
+    const password = loginPasswordInput.value;
+  
+    const Users = Parse.Object.extend("Users");
+    const query = new Parse.Query(Users);
+    query.equalTo("Username", username);
+    query.equalTo("Password", password);
+  
+    query.first().then(
+      (user) => {
+        if (user) {
+          // Login was successful
+          alert("Login successful!");
+          window.location.href = "dashboard.html"; // Redirect to the dashboard
+        } else {
+          // Login failed, display an error message
+          alert("Login failed: Invalid username or password");
+        }
+      },
+      (error) => {
+        // An error occurred during the query
+        alert("Login failed: " + error.message);
+      }
+    );
   }
-}
+  
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+  function handleSignup(event) {
+    event.preventDefault();
+  
+    const username = signupUsernameInput.value;
+    const email = signupEmailInput.value;
+    const password = signupPasswordInput.value;
+  
+    const Users = Parse.Object.extend("Users");
+    const user = new Users();
+    user.set("Username", username);
+    user.set("Password", password);
+    user.set("Email", email);
+  
+    user.save().then(
+      (user) => {
+        // Signup was successful
+        alert("Signup successful! You can now log in.");
+        // Optionally, you can automatically switch to the login form after successful registration.
+        // container.classList.remove("sign-up-mode");
+      },
+      (error) => {
+        // Signup failed, display an error message
+        alert("Signup failed: " + error.message);
+      }
+    );
+  }
+  
+
+  sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+  });
+
+  sign_in_btn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+  });
+
+  // Add a click event listener to the login button
+  loginBtn.addEventListener("click", handleLogin);
+signupBtn.addEventListener("click", handleSignup);
+
 });
-
-sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
-});
-
-// Add a click event listener to the login button
-loginBtn.addEventListener("click", handleLogin);
